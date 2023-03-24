eda9974a2d8f9212a4680b3c7fbd608aa249b099
Crowdbotics <team@crowdbotics.com>
2023-03-24 18:45:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nenad-testing-navig-39534",
+  url: "https://nenad-testing-navig-39534.botics.co",
+  api: "https://nenad-testing-navig-39534.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
