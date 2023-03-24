620886a9395c27f8e7feafa5974edcb3b41ec8f1
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-03-24 12:21:50 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +9,34 @@ const Untitled6 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Text style={styles.MljvSVwc}>Lorem ipsum…</Text><Text style={styles.sIsvneyu}>Lorem ipsum…</Text><Text style={styles.ZNpkPJeD}>Lorem ipsum…</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  MljvSVwc: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  sIsvneyu: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  ZNpkPJeD: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled6;
