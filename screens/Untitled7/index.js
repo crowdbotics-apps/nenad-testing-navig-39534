8c8db6107100abef4478dfc4dbaa837091f4a9ee
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-04-05 12:28:36 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled7/index.js
+++ screens/Untitled7/index.js
@@ -1,20 +1,33 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled7 = () => {
+  const navigation = useNavigation();
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Pressable onPress={() => {
+        navigation.navigate("Untitled1");
+      }}><Text style={styles.QsqoSruz}>Endless loading</Text></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  QsqoSruz: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled7;
